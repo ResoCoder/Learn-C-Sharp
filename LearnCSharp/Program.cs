f82968588b1b0945c57910ed40e2a9dff6c2a919
Matej Resetar <mresoo99@gmail.com>
2017-08-11 16:03:26 +0200

    Add project files.

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    class Person
+    {
+        public double Height;
+        public double Weight;
+        public string HairColor;
+
+        public Person(double height, double weight, string hairColor)
+        {
+            Height = height;
+            Weight = weight;
+            HairColor = hairColor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person John = new Person(180, 73.5, "brown");
+            Console.WriteLine(John.Weight);
+
+            Console.ReadLine();
+        }
+    }
+}
